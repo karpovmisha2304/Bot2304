d55ac6d544001a1bf5ad8ba60eebad9515dcb5f9
karpovmisha2304 <karpovmisha23041986@gmail.com>
2025-07-05 00:07:10 +0300

    Update bot.py
    Рабочий бот УГАДАЙ СЛОВО

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
-import random
+
+#API_URL = 'https://api.telegram.org/bot'                     #АПИШНИК ТЕЛЕГРАМ БОТА
+#BOT_TOKEN = '7702424432:AAGqdZn0OkH0BQVotnbpFp19WHtMBNQqfZ0' #ТОКЕН МОЕГО КОДА
+#TEXT = 'Ура! Классный апдейт!'
+#MAX_COUNTER = 100
+#print(message.from_user.first_name, message.from_user.last_name) 
+
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
+import random
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
 # полученный у @BotFather
@@ -13,8 +20,8 @@ bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
 # Количество попыток, доступных пользователю в игре
-ATTEMPTS = 5
-word_list = ['год', 'человек', 'время', 'дело', 'жизнь', 'день', 'рука', 'раз', 'работа', 'слово', 'место', 'лицо', 'друг', 'глаз', 'вопрос', 'дом', 'сторона', 'страна', 'мир', 'случай', 'голова', 'ребенок', 'сила', 'конец']
+ATTEMPTS = 6
+word_list = ['год', 'человек', 'время', 'дело', 'жизнь', 'день', 'рука']
 
 
 # Словарь, в котором будут храниться данные пользователя
@@ -29,10 +36,14 @@ def get_random_number() -> str:
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
+    #print(message.model_dump_json(indent=4, exclude_none=True))  #'name': ['result']['message']['from']['first_name']
+    
     await message.answer(
-        'Привет!\nДавайте сыграем в игру "Угадай число"?\n\n'
+        f'Привет! {message.from_user.first_name} {message.from_user.last_name}\nДавайте сыграем в игру "Угадай слово"?\n\n'
         'Чтобы получить правила игры и список доступных '
         'команд - отправьте команду /help'
+        ''
+        '\n\nДля согласия игры напиши \nда или нет'
     )
     # Если пользователь только запустил бота и его нет в словаре '
     # 'users - добавляем его в словарь
@@ -93,11 +104,14 @@ async def process_positive_answer(message: Message):
         users[message.from_user.id]['in_game'] = True
         users[message.from_user.id]['secret_number'] = get_random_number()
         users[message.from_user.id]['attempts'] = ATTEMPTS
+        users[message.from_user.id]['res'] = '*' * len(users[message.from_user.id]['secret_number'])
         await message.answer(
-            'Ура!\n\nЯ загадал число от 1 до 100, '
-            'попробуй угадать!'
+            f'Ура!\n\nЯ загадал слово из {len(users[message.from_user.id]["res"])} букв '
+            '\nпопробуй угадать !'
+            '\nНазови первую букву'
         )
     else:
+        print('YES')
         await message.answer(
             'Пока мы играем в игру я могу '
             'реагировать только на числа от 1 до 100 '
@@ -114,6 +128,7 @@ async def process_negative_answer(message: Message):
             'напишите об этом'
         )
     else:
+        print('NO')
         await message.answer(
             'Мы же сейчас с вами играем. Присылайте, '
             'пожалуйста, числа от 1 до 100'
@@ -121,7 +136,7 @@ async def process_negative_answer(message: Message):
 
 
 # Этот хэндлер будет срабатывать на отправку пользователем чисел от 1 до 100
-@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+@dp.message(F.text.len() == 1)
 async def process_numbers_answer(message: Message):
     if users[message.from_user.id]['in_game']:
         if not message.text.isalpha():
@@ -129,13 +144,16 @@ async def process_numbers_answer(message: Message):
         if len(message.text) != 1:
             await message.answer('Ошибка, нужно ввести только одну букву')
           
-        if message.text in users[message.from_user.id]['secret_number']:
-            users[message.from_user.id]['res'] = ''.join(users[message.from_user.id]['secret_number'][i] if message.text == users[message.from_user.id]['secret_number'][i] or users[message.from_user.id]['res'].isalpha() else '*' for i in (range(len(users[message.from_user.id]['secret_number']))))
-            await message.answer(f'Вот результат: {users[message.from_user.id]['res'].upper()}')
+        if message.text.lower() in users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['res'] = ''.join(users[message.from_user.id]['secret_number'][i] if users[message.from_user.id]['res'][i].isalpha() or message.text.lower() == users[message.from_user.id]['secret_number'][i] else '*' for i in (range(len(users[message.from_user.id]['secret_number']))))
+            print(users[message.from_user.id]["res"].upper())
+            await message.answer(
+                f'Вот результат: {users[message.from_user.id]["res"].upper()}'
+            )
 
         elif message.text not in users[message.from_user.id]['secret_number']:
             users[message.from_user.id]['attempts'] -= 1
-            await message.answer(f'Такой буквы нет. У вас осталось {users[message.from_user.id]['attempts']} попыток')
+            await message.answer(f'Такой буквы нет. У вас осталось {users[message.from_user.id]["attempts"]} попыток')
      
 
         if users[message.from_user.id]['secret_number'] == users[message.from_user.id]['res']:
@@ -143,9 +161,10 @@ async def process_numbers_answer(message: Message):
             users[message.from_user.id]['total_games'] += 1
             users[message.from_user.id]['wins'] += 1
             await message.answer(
-                f'Поздравляю Вы угадали слово {users[message.from_user.id]['secret_number']}'
-                f'И у вас осталось {users[message.from_user.id]['attempts']} попыток. '
+                f'Поздравляю Вы угадали слово {users[message.from_user.id]["secret_number"]}'
+                f'\n\nИ у вас осталось {users[message.from_user.id]["attempts"]} попыток. '
                 f'\n\nДавайте сыграем еще?'
+                '\n ДА или НЕТ?'
             )
         
         if users[message.from_user.id]['attempts'] == 0:
@@ -156,6 +175,7 @@ async def process_numbers_answer(message: Message):
                 f'попыток. Вы проиграли :(\n\nСлово '
                 f'было {users[message.from_user.id]["secret_number"]}'
                 f'\n\nДавайте сыграем еще?'
+                'ДА или НЕТ?'
             )
     else:
         await message.answer('Мы еще не играем. Хотите сыграть?')
